0c5304e26c740f8815e3b2a535ca949b5f0cfee6
rohitkumarstocktutor <rohit.kumar@stocktutor.in>
2025-09-21 07:49:02 +0530

    fix changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16491466128"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16491466128');
+            `,
+          }}
+        />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
       </body>
