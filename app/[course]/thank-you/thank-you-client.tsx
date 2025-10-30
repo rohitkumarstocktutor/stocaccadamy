ef5b3a0196fabe05930fee6e1318cfbaa2fe6cbf
rohitkumarstocktutor <rohit.kumar@stocktutor.in>
2025-10-30 09:34:44 +0530

    size chart and add to cart button added

--- app/[course]/thank-you/thank-you-client.tsx
+++ app/[course]/thank-you/thank-you-client.tsx
@@ -87,7 +87,7 @@ export default function ThankYouClient({ courseData, courseKey }: ThankYouClient
     if (workshopData?.wAurl) {
       window.open(workshopData.wAurl, '_blank');
     }else{
-      window.open("https://chat.whatsapp.com/GLvPantxs3pAjbbovvV1PH", '_blank');
+      window.open("https://chat.whatsapp.com/KtBDnhYGuWx4EbjwsjLDUW", '_blank');
     }
   };
 
